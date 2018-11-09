060750aa3b69e85c08b63d7bb5df6e1853084488
YuWei Chou <demoyuw@gmail.com>
2018-11-09 16:53:17 +0800

    自訂Admin UI 顯示Table欄位

--- website/app/admin.py
+++ website/app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Book_info, People_info
 
+
+class bookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'publish_date')
+
+
 # Register your models here.
-admin.site.register(Book_info)
+admin.site.register(Book_info, bookAdmin)
 admin.site.register(People_info)
+
+
